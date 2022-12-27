6c5182e8ade785d91e61d620dae4f82c91657212
camzzzzz <camille.hascoet@edu.ece.fr>
2022-12-27 19:25:32 +0100

    feat: orchestration avec kubernetes

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM node:18
 
-WORKDIR ./userapi
+WORKDIR /userapi
 
 COPY package*.json ./
 
--- userapi/src/dbClient.js
+++ userapi/src/dbClient.js
@@ -1,4 +1,4 @@
-var redis = require("redis");
+const redis = require('redis')
 const configure = require('./configure')
 
 const config = configure()
